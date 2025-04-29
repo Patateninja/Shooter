79baec4e706b877533a1342ae26652779088625d
Patateninja <80772495+patateninja@users.noreply.github.com>
2025-04-29 10:01:28 +0200

    dll

--- Shooter/Shooter/Main.cpp
+++ Shooter/Shooter/Main.cpp
@@ -1,4 +1,22 @@
+#include "SFML/Graphics.hpp"
+
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Window", sf::Style::Fullscreen);
+	sf::Event event;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+		window.clear();
+		window.display();
+	}
+
 	return 42;
 }
