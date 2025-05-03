7ed88db915efc77bf155ead32d0dd82b2409f592
Patateninja <80772495+Patateninja@users.noreply.github.com>
2025-05-03 13:09:59 +0200

    no more fullscreened

--- Shooter/Shooter/StateManager.cpp
+++ Shooter/Shooter/StateManager.cpp
@@ -2,7 +2,7 @@
 
 StateManager::StateManager(ResourceManager* _resourceManager)
 {
-	this->m_Window.Create(sf::VideoMode(1920,1080),"Shooter", sf::Style::Fullscreen);
+	this->m_Window.Create(sf::VideoMode(1920,1080),"Shooter", sf::Style::Default);
 	this->m_ResourceManager = _resourceManager;
 	this->m_CurrentState = new Menu(this, _resourceManager);
 	this->m_CurrentState->Init();
