9fcefdd180ef51f783cdf759ff623d147129a270
Patateninja <80772495+patateninja@users.noreply.github.com>
2025-06-10 17:33:29 +0200

    checkbox

---
+++ Shooter/Shooter/Checkbox.cpp
@@ -0,0 +1,40 @@
+#include "Checkbox.hpp"
+
+Checkbox::Checkbox(std::string _str, sf::Vector2f _pos, sf::Vector2f _size, sf::Texture* _texture, bool _active)
+{
+	this->m_Text.setString(_str);
+	this->m_Text.setFont(RscMana::Get<sf::Font>("Mono"));
+	this->m_Text.setPosition(_pos + sf::Vector2f(1.f, 1.f));
+	this->m_Rect.setPosition(_pos);
+	this->m_Rect.setSize(_size);
+	this->m_Rect.setTexture(_texture);
+
+	this->m_Active = _active;
+}
+
+void Checkbox::Update(Window& _window, bool& _bool)
+{
+	this->m_Active = _bool;
+
+	if (this->Button::Update(_window))
+	{
+		this->m_Active = !this->m_Active;
+	}
+
+	if (this->m_Active)
+	{
+		this->m_Rect.setFillColor(sf::Color::Green);
+	}
+	else
+	{
+		this->m_Rect.setFillColor(sf::Color::Red);
+	}
+
+	_bool = this->m_Active;
+}
+
+void Button::Display(Window& _window)
+{
+	_window.Draw(this->m_Rect);
+	_window.Draw(this->m_Text);
+}
